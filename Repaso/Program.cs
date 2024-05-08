e2b8d0b151048267714f23b60f61d1ddd11a32d9
mykea <mykea@JOSEJAVIER>
2024-05-08 08:00:06 -0700

    Add project files.

---
+++ Repaso/Program.cs
@@ -0,0 +1,8 @@
+﻿string miVariable;
+miVariable = "Jose";
+string materia = "Estructura de Datos";
+//miVariable = string.Format("Hello: {0} {1}", miVariable, materia);
+
+miVariable = $"Hello: {miVariable} {materia}" + 3;
+int x = 5;
+Console.WriteLine(miVariable);
